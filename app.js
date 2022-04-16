418e6cbfe683852d3498bc0b2e2fbea5824dfd3d
Nehal Ahmad <mohammadamiramir773@gmail.com>
2022-04-16 14:56:01 +0530

    add generator

--- app.js
+++ app.js
@@ -1,25 +1,16 @@
-//requiring path and fs modules
 const path = require('path');
 const fs = require('fs');
-//joining path of directory 
 const directoryPath = path.join(__dirname, 'tabler-icons');
-//passsing directoryPath and callback function
 fs.readdir(directoryPath, function (err, files) {
-    //handling error
     if (err) {
         return console.log('Unable to scan directory: ' + err);
     } 
-    //listing all files using forEach
-    const array = [];
-    var stream = fs.createWriteStream("types.txt");
+    var stream = fs.createWriteStream("generator/cmd/genbundle/bundle.txt");
     stream.once('open', function(fd) {
-      // stream.write("[\n");
       files.forEach(function (file) {
-        stream.write("'" + file + "',\n");
-        // stream.write("My second row\n");
-        // fs.writeFileSync("types.txt", file);
+        // stream.write("'" + file + "',\n");
+        stream.write("tabler/" + file.split('.svg')[0] + "\n");
       });
-      // stream.write("]");
       stream.end();
     });
 });
---
+++ generator/README.md
@@ -0,0 +1,7 @@
+=== generate bundle
+
+list the iconst that should be bundled in:  `cmd/genbundle/bundle.txt` then:
+
+```
+go run cmd/genbundle/main.go > iconBundle.ts
+```
